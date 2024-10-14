b93564dfa4a2064a3a7838103229ca9c655622c7
oops985 <60307819+oops985@users.noreply.github.com>
2024-10-14 20:32:20 +0800

    Update 1.modify.sh

--- preset-immortalwrt/1.modify.sh
+++ preset-immortalwrt/1.modify.sh
@@ -2,7 +2,8 @@
 
 # modify login IP
 #sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
-sed -i 's/OpenWrt/XiaoMi/g' package/base-files/files/bin/config_generate
+#sed -i 's/OpenWrt/XiaoMi/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/XiaoMi/g' package/base-files/files/bin/config_generate
 
 # replace geodata source
 . $(dirname $0)/../extra-files/update-geodata.sh
